1853cdc414f11a4f731b488b65cbc467bd0d8bf8
Aiman Azmie <aimanaaw@gmail.com>
2019-09-24 17:56:46 +0000

    Committing the spinner exercise

---
+++ spinner.js
@@ -0,0 +1,40 @@
+
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 200);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 300);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 400);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r|   '); 
+}, 500);
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 600);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 700);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 800);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r|   '); 
+}, 900);
